0595e5901cae477f5902c34f8abb6da07b8a974c
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-08 03:34:36 +0800

    feat: get recipe by index.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const PORT = process.argv[2];
+const app = express();
+const FILENAME = './data.json';
+
+const handleQueryParams = (request, response) => {
+  read(FILENAME, (err, data) => {
+    // Respond with the name at the index specified in the URL
+    console.log('Recipe:', data.recipes[request.params.index]);
+    response.send(data.recipes[request.params.index]);
+  });
+};
+
+app.get('/recipe/:index', handleQueryParams);
+
+app.listen(PORT);
